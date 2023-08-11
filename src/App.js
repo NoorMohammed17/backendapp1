1b94555afc4d3df9e5d79f061def6c6cb9f21ea6
NoorMohammed17 <noormuhammads1797@gmail.com>
2023-08-11 20:25:46 +0530

    app.js modified with error data

--- src/App.js
+++ src/App.js
@@ -1,47 +1,55 @@
-import React, { useState } from "react";
+import React, { useState, useCallback } from "react";
 
 import MoviesList from "./components/MoviesList";
 import "./App.css";
 
 function App() {
   const [movies, setMovies] = useState([]);
-  const [isLoading , setIsLoading] = useState(false);
-  // ***********************USING .THEN  METHOD TO FETCH DATA *******************************//
-  // const fetchMoviesHandler = () => {
-  //   fetch("https://swapi.dev/api/films/")
-  //     .then((response) => {
-  //       return response.json();
-  //     })
-  //     .then((data) => {
-  //       const tranfomedMoviesData = data.results.map((movieData) => {
-  //         return {
-  //           id: movieData.episode_id,
-  //           title: movieData.title,
-  //           openingText: movieData.opening_crawl,
-  //           releaseDate: movieData.release_date,
-  //         };
-  //       });
-  //       console.log(tranfomedMoviesData)
-  //       setMovies(tranfomedMoviesData);
-  //     });
-  // };
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
 
-  //***********************using async await method to fetch data ***************************//
-  async function fetchMoviesHandler() {
-    setIsLoading(true)
-    const response = await fetch("https://swapi.dev/api/films/");
-    const data = await response.json();
-    const tranfomedMoviesData = data.results.map((movieData) => {
-      return {
-        id: movieData.episode_id,
-        title: movieData.title,
-        openingText: movieData.opening_crawl,
-        releaseDate: movieData.release_date,
-      };
-    });
-    console.log(tranfomedMoviesData);
-    setMovies(tranfomedMoviesData);
-    setIsLoading(false)
+  const fetchMoviesHandler = useCallback(async () => {
+    setIsLoading(true);
+    setError(null);
+    try {
+      const response = await fetch("https://swapi.dev/api/films/");
+      if (!response.ok) {
+        throw new Error("Something went wrong!");
+      }
+
+      const data = await response.json();
+
+      const transformedMovies = data.results.map((movieData) => {
+        return {
+          id: movieData.episode_id,
+          title: movieData.title,
+          openingText: movieData.opening_crawl,
+          releaseDate: movieData.release_date,
+        };
+      });
+      setMovies(transformedMovies);
+    } catch (error) {
+      setError(error.message);
+    }
+    setIsLoading(false);
+  }, []);
+
+  // useEffect(() => {
+  //   fetchMoviesHandler();
+  // }, [fetchMoviesHandler]);
+
+  let content = <p>Found no movies.</p>;
+
+  if (movies.length > 0) {
+    content = <MoviesList movies={movies} />;
+  }
+
+  if (error) {
+    content = <p>{error}</p>;
+  }
+
+  if (isLoading) {
+    content = <p>Loading...</p>;
   }
 
   return (
@@ -49,13 +57,31 @@ function App() {
       <section>
         <button onClick={fetchMoviesHandler}>Fetch Movies</button>
       </section>
-      <section>
-         {!isLoading && movies.length>0 && <MoviesList movies={movies} />}
-         {!isLoading && movies.length === 0 && <p>No movies found</p>}
-         {isLoading && <h1>Loading...</h1>}
-      </section>
+      <section>{content}</section>
     </React.Fragment>
   );
 }
 
 export default App;
+
+// ***********************USING .THEN  METHOD TO FETCH DATA *******************************//
+// const fetchMoviesHandler = () => {
+//   fetch("https://swapi.dev/api/films/")
+//     .then((response) => {
+//       return response.json();
+//     })
+//     .then((data) => {
+//       const tranfomedMoviesData = data.results.map((movieData) => {
+//         return {
+//           id: movieData.episode_id,
+//           title: movieData.title,
+//           openingText: movieData.opening_crawl,
+//           releaseDate: movieData.release_date,
+//         };
+//       });
+//       console.log(tranfomedMoviesData)
+//       setMovies(tranfomedMoviesData);
+//     });
+// };
+
+//***********************using async await method to fetch data ***************************//
